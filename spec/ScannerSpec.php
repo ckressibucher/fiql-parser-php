bd3423ff8a6f5cd1510c1fbc86d670560f14d1ff
Claudio Kressibucher <ckressibucher@inmedias.com>
2015-04-15 02:28:17 +0200

    implemented iterative scanner (instead of recursive), added token type to list of tokens

--- spec/ScannerSpec.php
+++ spec/ScannerSpec.php
@@ -4,6 +4,7 @@ namespace spec\Ckr\Fiql;
 
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
+use Ckr\Fiql\Scanner;
 
 class ScannerSpec extends ObjectBehavior
 {
@@ -14,61 +15,108 @@ class ScannerSpec extends ObjectBehavior
 
     function it_returns_an_array_of_tokens()
     {
-        $this->scan('selector')->shouldBeEqualTo(['selector']);
+        $this->scan('selector')->shouldBeEqualTo([
+            [Scanner::T_SELECTOR, 'selector']
+        ]);
     }
 
     function it_recognizes_a_selector()
     {
         /* @see https://tools.ietf.org/html/rfc3986#section-2.3 */
         $exampleSelector = 'lettersALPHA456-._~';
-        $this->scan($exampleSelector)->shouldBeEqualTo([$exampleSelector]);
+        $this->scan($exampleSelector)->shouldBeEqualTo([
+            [Scanner::T_SELECTOR, $exampleSelector]
+        ]);
     }
 
     function it_recognizes_a_constraint()
     {
         $exampleConstraint = 'a==b';
-        $this->scan($exampleConstraint)->shouldBeEqualTo(['a', '==', 'b']);
+        $this->scan($exampleConstraint)->shouldBeEqualTo([
+            [Scanner::T_SELECTOR, 'a'],
+            [Scanner::T_COMP_OPERATOR, '=='],
+            [Scanner::T_ARGUMENT, 'b']
+        ]);
     }
 
     function it_recognizes_a_group()
     {
         $exampleGroup = '(selector)';
-        $this->scan($exampleGroup)->shouldBeEqualTo(['(', 'selector', ')']);
+        $this->scan($exampleGroup)->shouldBeEqualTo([
+            [Scanner::T_GROUP_START, '('],
+            [Scanner::T_SELECTOR, 'selector'],
+            [Scanner::T_GROUP_END, ')'],
+        ]);
+    }
+
+    function it_recognizes_a_double_group()
+    {
+        $source = '((select))';
+        $this->scan($source)->shouldBeEqualTo([
+            [Scanner::T_GROUP_START, '('],
+            [Scanner::T_GROUP_START, '('],
+            [Scanner::T_SELECTOR, 'select'],
+            [Scanner::T_GROUP_END, ')'],
+            [Scanner::T_GROUP_END, ')'],
+        ]);
     }
 
     function it_recognizes_an_and_operator()
     {
         $example = 'a;b';
-        $this->scan($example)->shouldBeEqualTo(['a', ';', 'b']);
+        $this->scan($example)->shouldBeEqualTo([
+            [Scanner::T_SELECTOR, 'a'],
+            [Scanner::T_BOOL_OPERATOR, ';'],
+            [Scanner::T_SELECTOR, 'b']
+        ]);
     }
 
     function it_recognizes_an_or_operator()
     {
         $example = 'a,b';
-        $this->scan($example)->shouldBeEqualTo(['a', ',', 'b']);
+        $this->scan($example)->shouldBeEqualTo([
+            [Scanner::T_SELECTOR, 'a'],
+            [Scanner::T_BOOL_OPERATOR, ','],
+            [Scanner::T_SELECTOR, 'b']
+        ]);
+    }
+
+
+    function it_allows_multiple_bool_operators_without_parentheses()
+    {
+        $source = 'a;b;c==5';
+        $this->scan($source)->shouldBeEqualTo([
+            [Scanner::T_SELECTOR, 'a'],
+            [Scanner::T_BOOL_OPERATOR, ';'],
+            [Scanner::T_SELECTOR, 'b'],
+            [Scanner::T_BOOL_OPERATOR, ';'],
+            [Scanner::T_SELECTOR, 'c'],
+            [Scanner::T_COMP_OPERATOR, '=='],
+            [Scanner::T_ARGUMENT, '5'],
+        ]);
     }
 
     function it_tokenizes_a_complex_example()
     {
         $example = 'abc==5;(xyz=gt=3,field!=value;(checkFlag))';
         $this->scan($example)->shouldBeEqualTo([
-            'abc',
-            '==',
-            '5',
-            ';',
-            '(',
-            'xyz',
-            '=gt=',
-            '3',
-            ',',
-            'field',
-            '!=',
-            'value',
-            ';',
-            '(',
-            'checkFlag',
-            ')',
-            ')'
+            [Scanner::T_SELECTOR, 'abc'],
+            [Scanner::T_COMP_OPERATOR, '=='],
+            [Scanner::T_ARGUMENT, '5'],
+            [Scanner::T_BOOL_OPERATOR, ';'],
+            [Scanner::T_GROUP_START, '('],
+            [Scanner::T_SELECTOR, 'xyz'],
+            [Scanner::T_COMP_OPERATOR, '=gt='],
+            [Scanner::T_ARGUMENT, '3'],
+            [Scanner::T_BOOL_OPERATOR, ','],
+            [Scanner::T_SELECTOR, 'field'],
+            [Scanner::T_COMP_OPERATOR, '!='],
+            [Scanner::T_ARGUMENT, 'value'],
+            [Scanner::T_BOOL_OPERATOR, ';'],
+            [Scanner::T_GROUP_START, '('],
+            [Scanner::T_SELECTOR, 'checkFlag'],
+            [Scanner::T_GROUP_END, ')'],
+            [Scanner::T_GROUP_END, ')'],
         ]);
     }
 }
--- src/Scanner.php
+++ src/Scanner.php
@@ -22,10 +22,13 @@ class Scanner
     const OP_AND = '&&';
     const OP_OR = '||';
 
-    /**
-     * @var string
-     */
-    protected $remaining;
+    // token types
+    const T_GROUP_START = 1;    // '('
+    const T_GROUP_END = 2;      // ')'
+    const T_COMP_OPERATOR = 10;
+    const T_BOOL_OPERATOR = 15;
+    const T_SELECTOR = 20;
+    const T_ARGUMENT = 25;
 
     /**
      * Regex for unreserved characters as described in
@@ -81,6 +84,8 @@ class Scanner
      */
     protected static $regexSelector;
 
+    protected static $regexConstraint;
+
 
     /**
      * key: FIQL comparison operator
@@ -105,17 +110,10 @@ class Scanner
      *
      * @var array
      */
-    protected $logicalOperators = [
+    protected $boolOperators = [
         ';' => self::OP_AND,
         ',' => self::OP_OR
     ];
-
-    /**
-     * Stores the next token.
-     *
-     * @var string
-     */
-    protected $nextToken;
 
     /**
      * List of already parsed tokens.
@@ -125,30 +123,14 @@ class Scanner
     protected $tokens;
 
     /**
+     * @var int
+     */
+    protected $level;
+
+    /**
      * @var bool
      */
     protected static $isInitialized = false;
-
-    /**
-     * Main method to tokenize a given FIQL Expression.
-     *
-     * @param   string $queryString
-     * @return  string[]
-     * @throws  SyntaxException
-     */
-    public function scan($queryString)
-    {
-        static::initialize();
-        $this->remaining = $queryString;
-        $this->tokens = [];
-        $this->scanExpression();
-        if ($this->remaining !== '') {
-            throw new SyntaxException(
-                'Remaining string is expected to be empty after parsing the expression'
-            );
-        }
-        return $this->tokens;
-    }
 
     /**
      * Initializes combined regex patterns.
@@ -165,192 +147,109 @@ class Scanner
                 . '=)';
             static::$regexArgument = '(?<arg>' . static::$regexArgChar . '+)';
 
+            static::$regexConstraint = '/' . static::$regexSelector
+                . '(' . static::$regexCompareOp . static::$regexArgument . ')?/';
+
             static::$isInitialized = true;
         }
     }
 
     /**
-     * Scans an expression from the beginning of the remaining string.
+     * Main method to tokenize a given FIQL Expression.
      *
-     * @throws SyntaxException
-     */
-    protected function scanExpression()
-    {
-        if ($isOpenGroup = $this->isGroupStart()) {
-            $this->shiftToken();
-        }
-        if ($this->isSelector()) {
-            $this->scanConstraint();
-        } else {
-            // must be a sub expression
-            if (!$isOpenGroup) {
-                // if no group was started, then the first token must be a
-                // selector, starting a constraint
-                throw new SyntaxException('Selector was expected');
-            }
-            $this->scanExpression();
-        }
-        while ($this->isLogicalOperator()) {
-            $this->shiftToken();
-            $this->scanExpression();
-        }
-        if ($isOpenGroup) {
-            $regex = '\)';
-            $this->expect($regex); // close group
-            $this->shiftToken();
-        }
-    }
-
-    /**
-     * Scans a constraint from the beginning of the remaining string.
-     *
-     * @throws SyntaxException
-     */
-    protected function scanConstraint()
-    {
-        if (!$this->isSelector()) {
-            throw new SyntaxException('Expected a selector');
-        }
-        $this->shiftToken();
-
-        if ($this->isComparisonOperator()) {
-            $this->shiftToken();
-            if (!$this->isArgument()) {
-                throw new SyntaxException('An argument token was expected');
-            }
-            $this->shiftToken();
-        }
-    }
-
-    /**
-     * Checks if the given regex matches the beginning of the remaining string.
-     * If so, it is set to nextToken. Otherwise, an exception is thrown.
-     *
-     * @param   string  $regexExpected
+     * @param   string $queryString
+     * @return  string[]
      * @throws  SyntaxException
      */
-    protected function expect($regexExpected)
+    public function scan($queryString)
     {
-        $pattern = '/^(?<token>' . $regexExpected . ')/';
-        if (preg_match($pattern, $this->remaining, $matches)) {
-            $this->nextToken = $matches['token'];
-            return;
+        static::initialize();
+        $this->tokens = [];
+        $this->level = 0;
+
+        do {
+            list($pre, $selector, $compOp, $arg, $post) = $this->matchConstraint($queryString);
+            $this->processPreamble($pre);
+            if ($selector) {
+                $this->tokens[] = [self::T_SELECTOR, $selector];
+            }
+            if ($compOp) {
+                $this->tokens[] = [self::T_COMP_OPERATOR, $compOp];
+                $this->tokens[] = [self::T_ARGUMENT, $arg];
+            }
+            $queryString = $post;
+        } while ($queryString !== '');
+
+        if ($this->level !== 0) {
+            throw new SyntaxException('Parenthesis were not opened/closed correctly');
         }
-        throw new SyntaxException('Expected pattern ' . $regexExpected . ' did not match');
+
+        return $this->tokens;
     }
 
     /**
-     * Checks if the remaining string starts with a group opening parenthesis,
-     * and, if so, sets this character to `nextToken` and returns true.
-     * Otherwise, false is returned.
-     *
-     * @return bool
+     * @param $source
+     * @return array
+     * @throws SyntaxException
      */
-    protected function isGroupStart()
+    protected function matchConstraint($source)
     {
-        if (strlen($this->remaining) === 0) {
-            return false;
+        if (preg_match(static::$regexConstraint, $source, $matches)) {
+            $constraint = $matches[0];
+            $pos = strpos($source, $constraint);
+            $pre = substr($source, 0, $pos);
+            $postLen = strlen($source) - ($pos + strlen($constraint));
+            if ($postLen > 0) {
+                $post = substr($source, $pos + strlen($constraint));
+            } else {
+                $post = '';
+            }
+            $selector = $matches['selector'];
+            if (isset($matches['comp_op'])) {
+                $compOp = $matches['comp_op'];
+                $arg = $matches['arg'];
+            } else {
+                $compOp = $arg = null;
+            }
+            return [
+                $pre,
+                $selector,
+                $compOp,
+                $arg,
+                $post
+            ];
+        } elseif (preg_match('/^\)*$/', $source)) {
+            // end of expression: closing parenthesis
+            return [$source, null, null, null, ''];
+        } else {
+            throw new SyntaxException('source did not match a valid constraint');
         }
-        if ($this->remaining[0] === '(') {
-            $this->nextToken = '(';
-            return true;
-        }
-        return false;
     }
 
     /**
-     * Tries to parse a selector from the beginning of the remaining string.
-     * Sets nextToken to the selector value and returns true, it it succeeds,
-     * else false.
-     *
-     * @return bool
+     * @param string $preamble
      */
-    protected function isSelector()
+    protected function processPreamble($preamble)
     {
-        $pattern = '/^' . self::$regexSelector . '/';
-        if (preg_match($pattern, $this->remaining, $matches)) {
-            $this->nextToken = $matches['selector'];
-            return true;
-        }
-        return false;
-    }
-
-    /**
-     * Checks if the next token is a comparison operator. Sets `nextToken`
-     * and returns true, if it is. Else, false is returned.
-     *
-     * @return bool
-     */
-    protected function isComparisonOperator()
-    {
-        $pattern = '/^' . self::$regexCompareOp . '/';
-        if (preg_match($pattern, $this->remaining, $matches)) {
-            $comp = $matches['comp_op'];
-            if (isset($this->compareOperators[$comp])) {
-                $this->nextToken = $comp;
-                return true;
+        for ($i = 0; $i < strlen($preamble); $i++) {
+            $char = $preamble[$i];
+            switch ($char) {
+                case '(':
+                    $this->tokens[] = [self::T_GROUP_START, $char];
+                    $this->level++;
+                    break;
+                case ')';
+                    $this->tokens[] = [self::T_GROUP_END, $char];
+                    $this->level--;
+                    break;
+                case ';':
+                case ',':
+                $this->tokens[] = [self::T_BOOL_OPERATOR, $char];
+                    break;
+                default:
+                    new SyntaxException();
             }
         }
-        return false;
     }
 
-    /**
-     * Checks if the next token is a logical operator. If so,
-     * `nextToken` is set to the operator string, and true is
-     * returned. Otherwise, false is returned.
-     *
-     * @return bool
-     */
-    protected function isLogicalOperator()
-    {
-        if (strlen($this->remaining) === 0) {
-            return false;
-        }
-        $operators = array_flip($this->logicalOperators);
-        if (in_array($this->remaining[0], $operators)) {
-            $this->nextToken = $this->remaining[0];
-            return true;
-        }
-    }
-
-    /**
-     * Checks if the next token is an argument. If so, `nextToken`
-     * is set to the argument value, and true is returned. Otherwise,
-     * false is returned.
-     *
-     * @return bool
-     */
-    protected function isArgument()
-    {
-        $pattern = '/^' . self::$regexArgument . '/';
-        if (preg_match($pattern, $this->remaining, $matches)) {
-            $this->nextToken = $matches['arg'];
-            return true;
-        }
-        return false;
-    }
-
-    /**
-     * appends nextToken to tokens array and strips this token
-     * from the remaining string
-     */
-    protected function shiftToken()
-    {
-        $this->tokens[] = $this->nextToken;
-        $this->reduceRemaining();
-    }
-
-    /**
-     * strips the last token from beginning of remaining string
-     */
-    protected function reduceRemaining()
-    {
-        $idxLast = count($this->tokens) - 1;
-        $len = strlen($this->tokens[$idxLast]);
-        if ($len === strlen($this->remaining)) {
-            $this->remaining = '';
-        } else {
-            $this->remaining = substr($this->remaining, $len);
-        }
-    }
 }
