f336717967d756d6fc7e18b83f806abb0cf28d7d
Claudio Kressibucher <ckressibucher@inmedias.com>
2015-04-30 22:04:00 +0200

    implemented BoolExpr node

--- spec/Tree/Node/BoolExprSpec.php
+++ spec/Tree/Node/BoolExprSpec.php
@@ -2,13 +2,40 @@
 
 namespace spec\Ckr\Fiql\Tree\Node;
 
+use Ckr\Fiql\Tree\Node;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
 class BoolExprSpec extends ObjectBehavior
 {
-    function it_is_initializable()
+
+    function it_is_initializable(Node $left, Node $right)
     {
+        $this->beConstructedWith($left, Node\BoolExpr::OP_AND, $right);
         $this->shouldHaveType('Ckr\Fiql\Tree\Node\BoolExpr');
+    }
+
+    function it_implements_the_node_interface(Node $left, Node $right)
+    {
+        $this->beConstructedWith($left, Node\BoolExpr::OP_AND, $right);
+        $this->shouldHaveType('Ckr\Fiql\Tree\Node');
+    }
+
+    function its_getLeftOperand_returns_the_left_node(Node $left, Node $right)
+    {
+        $this->beConstructedWith($left, Node\BoolExpr::OP_AND, $right);
+        $this->getLeftOperand()->shouldReturn($left);
+    }
+
+    function its_getRightOperand_returns_the_left_node(Node $left, Node $right)
+    {
+        $this->beConstructedWith($left, Node\BoolExpr::OP_OR, $right);
+        $this->getRightOperand()->shouldReturn($right);
+    }
+
+    function its_getOperator_returns_the_operator(Node $left, Node $right)
+    {
+        $this->beConstructedWith($left, Node\BoolExpr::OP_OR, $right);
+        $this->getOperator()->shouldReturn(Node\BoolExpr::OP_OR);
     }
 }
--- src/Tree/Node/BoolExpr.php
+++ src/Tree/Node/BoolExpr.php
@@ -2,13 +2,54 @@
 
 namespace Ckr\Fiql\Tree\Node;
 
+use Ckr\Fiql\Tree\Node;
+
 class BoolExpr extends AbstractNode
 {
+
+
+    const OP_AND = '&&';
+
+    const OP_OR = '||';
+
+    private $operator;
+
+    private $left;
+
+    private $right;
+
+    public function __construct(Node $left, $operator, Node $right)
+    {
+        if (!in_array($operator, [self::OP_OR, self::OP_AND])) {
+            throw new \InvalidArgumentException(
+                'operator must be one of ' . self::OP_OR . ' and ' . self::OP_AND
+            );
+        }
+        $this->operator = $operator;
+        $this->left = $left;
+        $this->right = $right;
+    }
+
     /**
      * {@inheritDoc}
      */
     public function getType()
     {
         return 'bool_expr';
+    }
+
+    public function getLeftOperand()
+    {
+        return $this->left;
+    }
+
+    public function getRightOperand()
+    {
+        return $this->right;
+    }
+
+    public function getOperator()
+    {
+        return $this->operator;
     }
 }
